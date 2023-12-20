2cc75058fae30435a760b3fba90dcc3ad15e3f66
dann9109 <danielxstella@gmail.com>
2023-12-20 18:22:13 -0500

    character prompt save

--- Develop/script.js
+++ Develop/script.js
@@ -1,129 +1,55 @@
-// // Assignment Code
-// var generateBtn = document.querySelector("#generate");
-
-// var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
-
-// var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-
-// var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '=', '[', ']', '{', '}', '|', '\\', ';', ':', "'", '"', '<', '>', ',', '.', '/', '?'];
-
-// var numbers = [1, 2, 4, 5, 6, 7, 8, 9, 10]
-
-// var characterSet = [];
-
-
-// var password = ''
-
-
-
-
-
-// // Write password to the #password input
-// // function writePassword() {
-// //   var password = generatePassword();
-// //   var passwordText = document.querySelector("#password");
-
-// //   passwordText.value = password;
-
-// // }
-// // textBox.value = pass;
-
-// var passwordLength = +prompt('Please provide a password length');
-
-// var wantsLowercase = true;
-// var wantsUppercase = true;
-// var wantsNumbers = true;
-// var wantsSpecialCharacters = false;
-
-// var textBox = document.querySelector('form textarea')
-
-// var pass = generatePassword();
-
-// function generatePassword() {
-
-
-//   // if the user wants lowercase, then we combine the character set with lowercase array
-//   if (wantsLowercase) {
-//     characterSet = characterSet.concat(lowercase)
-
-//   }
-//   if (wantsUppercase) {
-//     characterSet = characterSet.concat(uppercase)
-
-//   }
-//   if (wantsNumbers) {
-//     characterSet = characterSet.concat(numbers)
-
-//   }
-//   if (wantsSpecialCharacters) {
-//     characterSet = characterSet.concat(specialCharacters)
-
-//   }
-
-//   // console.log(characterSet);
-
-//   for (var count = 0; count < passwordLength; count++) {
-//     var ranIndex = Math.floor(Math.random() * characterSet.length);
-//     password += characterSet[ranIndex];
-//   }
-
-//   return password;
-// }
-// generateBtn.addEventListener("click", function () {
-//   generatePassword();
-// });
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", generatePassword);
-
-
-
-// console.log(pass);
-
+// Select the generate button element from the DOM
 var generateBtn = document.querySelector("#generate");
+
+// Define arrays for different character sets
 var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '=', '[', ']', '{', '}', '|', '\\', ';', ':', "'", '"', '<', '>', ',', '.', '/', '?'];
-var numbers = [1, 2, 4, 5, 6, 7, 8, 9, 10]
+var numbers = [1, 2, 4, 5, 6, 7, 8, 9, 10];
+
+// Initialize variables for character set, password, password length, and user choices
 var characterSet = [];
-var password = ''
-
+var password = '';
 var passwordLength = +prompt('Please provide a password length');
-var wantsLowercase = true;
-var wantsUppercase = true;
-var wantsNumbers = true;
-var wantsSpecialCharacters = false;
+var wantsLowercase = confirm('Include lowercase characters?');
+var wantsUppercase = confirm('Include uppercase characters?');
+var wantsNumbers = confirm('Include numbers?');
+var wantsSpecialCharacters = confirm('Include special characters?');
 
+// Select the password text box element from the DOM
 var textBox = document.querySelector('#password');
 
+// Function to generate the password
 function generatePassword() {
   // Reset password and characterSet before generating a new password
   password = '';
   characterSet = [];
 
+  // Check user choices and add respective character sets to the main characterSet
   if (wantsLowercase) {
-    characterSet = characterSet.concat(lowercase)
+    characterSet = characterSet.concat(lowercase);
   }
   if (wantsUppercase) {
-    characterSet = characterSet.concat(uppercase)
+    characterSet = characterSet.concat(uppercase);
   }
   if (wantsNumbers) {
-    characterSet = characterSet.concat(numbers)
+    characterSet = characterSet.concat(numbers);
   }
   if (wantsSpecialCharacters) {
-    characterSet = characterSet.concat(specialCharacters)
+    characterSet = characterSet.concat(specialCharacters);
   }
 
+  // Generate the password by randomly selecting characters from the characterSet
   for (var count = 0; count < passwordLength; count++) {
     var ranIndex = Math.floor(Math.random() * characterSet.length);
     password += characterSet[ranIndex];
   }
 
-  textBox.value = password; // Set the value of the textarea to the generated password
+  // Set the value of the textarea to the generated password
+  textBox.value = password;
 }
 
+// Add an event listener to the generate button to call the generatePassword function when clicked
 generateBtn.addEventListener("click", generatePassword);
-
-
 
 
